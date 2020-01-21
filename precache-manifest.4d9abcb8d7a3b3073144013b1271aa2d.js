2c55fbdab4838cd7f6e184c2912fc71535610583
Leoul Tilahun <33269725+PrinceUmbrella@users.noreply.github.com>
2020-01-21 10:22:56 -0500

    Updates

---
+++ precache-manifest.4d9abcb8d7a3b3073144013b1271aa2d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe2a27bd1516e6135f7c70ae85d0e04",
+    "url": "/personalwebsite/index.html"
+  },
+  {
+    "revision": "962a144750a1b4f115a8",
+    "url": "/personalwebsite/static/css/2.4e007aa2.chunk.css"
+  },
+  {
+    "revision": "018206c0c63829f13f86",
+    "url": "/personalwebsite/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "962a144750a1b4f115a8",
+    "url": "/personalwebsite/static/js/2.f73e064e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/personalwebsite/static/js/2.f73e064e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "018206c0c63829f13f86",
+    "url": "/personalwebsite/static/js/main.ffbe19a3.chunk.js"
+  },
+  {
+    "revision": "ce3f5dcc3187fe2ab4b1",
+    "url": "/personalwebsite/static/js/runtime-main.3af19a45.js"
+  },
+  {
+    "revision": "48cee99c6336d3b7fb3f38e52eabd6ba",
+    "url": "/personalwebsite/static/media/MicrosoftLogo.48cee99c.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/personalwebsite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/personalwebsite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/personalwebsite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/personalwebsite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/personalwebsite/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a92fd52317f08b7172ed427063e9d81c",
+    "url": "/personalwebsite/static/media/googleLogo.a92fd523.png"
+  },
+  {
+    "revision": "c7de0c0581a094b56b84185b12cd685d",
+    "url": "/personalwebsite/static/media/profileimage.c7de0c05.jpg"
+  },
+  {
+    "revision": "12f0aa4411e8efd60de34b1d13dddbd5",
+    "url": "/personalwebsite/static/media/react.12f0aa44.jpg"
+  }
+]);
